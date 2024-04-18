ff34cec4aa1f5272b54209d334ddd814760df4c6
VRodriguez7 <Victor.rodriguez@its.ny.gov>
2024-04-18 14:19:37 -0400

    MOVEUP

--- 07-multiple-stages/Starter/Spinner/SpinnerView.swift
+++ 07-multiple-stages/Starter/Spinner/SpinnerView.swift
@@ -36,45 +36,64 @@ struct SpinnerView: View {
   struct Leaf: View {
     let rotation: Angle
     let isCurrent: Bool
+    let isCompleteing: Bool
+   
 
     var body: some View {
       Capsule()
         .stroke(isCurrent ? Color.white : .gray, lineWidth: 8)
-        .frame(width: 20, height: 50)
+        .frame(width: 20, height: isCompleteing ? 20 : 50)
         .offset(
           isCurrent
             ? .init(width: 10, height: 0)
             : .init(width: 40, height: 70)
         )
         .scaleEffect(isCurrent ? 0.5 : 1)
-        .rotationEffect(rotation)
-      // MARK: - Update deprecated `animation` modifier
-      //        .animation(.easeIn(duration: 1.5))
-      // Animate when `isCurrent` changes
+        .rotationEffect(isCompleteing ? .zero : rotation)
         .animation(.easeInOut(duration: 1.5), value: isCurrent)
     }
   }
 
   let leavesCount = 12
   @State var currentIndex = -1
+    @State var completed = false
+    @State var isVisible = true
+    let shootUp = AnyTransition.offset(x:0 , y: -1000)
+        .animation(.easeIn(duration: 1))
   
-  var body: some View {
-    VStack {
-      ZStack {
-        ForEach(0..<leavesCount) { index in
-          Leaf(
-            rotation: .init(degrees: .init(index) / .init(leavesCount) * 360),
-            isCurrent: index == currentIndex
-          )
+    var body: some View {
+        VStack {
+            if isVisible {
+                ZStack {
+                    ForEach(0..<leavesCount) { index in
+                        Leaf(
+                            rotation: .init(degrees: .init(index) / .init(leavesCount) * 360),
+                            isCurrent: index == currentIndex,
+                            isCompleteing: completed
+                        )
+                    }
+                }
+                .transition(shootUp)
+                .onAppear(perform: animate)
+            }
         }
-      }
-      .onAppear(perform: animate)
     }
-  }
-  
   func animate() {
+      var iteration = 0
+      @State var completed = false
+      
     Timer.scheduledTimer(withTimeInterval: 0.15, repeats: true) { timer in
       currentIndex = (currentIndex + 1) % leavesCount
+        
+        iteration += 1
+        if iteration == 30 {
+            timer.invalidate()
+            completed = true
+            currentIndex = -1
+            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+                isVisible = false
+            }
+        }
     }
   }
 }
